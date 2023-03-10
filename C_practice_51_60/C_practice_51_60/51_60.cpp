93918aae864670bbf2ed256bc41ebdad1e605818
1434817639 <122460446+1434817639@users.noreply.github.com>
2023-03-10 10:51:41 +0800

    Add files via upload

---
+++ C_practice_51_60/C_practice_51_60/51_60.cpp
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <conio.h>
+#include <stdlib.h>
+
+
+
+
+//第五十六题---第六十题
+//int main()
+//{
+//	
+//	return 0;
+//}
+
+
+//第五十五题
+//int main()
+//{
+//	int a, b;
+//	a = 1;
+//	b = ~a;
+//	printf("\40:The a's 1 complement(decimal) is %d\n",b);
+//	a = ~a;
+//	printf("\40:The a's 1 complement(hexidecimal) is %x\n", a);
+//	return 0;
+//}
+
+
+//第五十四题
+//int main()
+//{
+//	unsigned a, b, c, d;
+//	scanf("%o",&a);
+//	b = a >> 4;
+//	c = ~(~0 << 4);
+//	d = b & c;
+//	printf("%o\n%o\n",a,d);
+//	return 0;
+//}
+
+
+//第五十三题
+//int main()
+//{
+//	int i, j, m, n;
+//	i = 0 ^ 0;
+//	j = 0 ^ 1;
+//	m = 1 ^ 0;
+//	n = 1 ^ 1;
+//	printf("i = %d\n",i);
+//	printf("j = %d\n", j);
+//	printf("m = %d\n", m);
+//	printf("n = %d\n", n);
+//	return 0;
+//}
+
+//第五十二题
+//int main()
+//{
+//	int a, b;
+//	a = 077;
+//	b = a | 3;
+//	printf("\40: The a & b(decimal) is %d\n",b);
+//	b |= 7;
+//	printf("\40: The a&b(decimal) is %d\n",b);
+//	return 0;
+//}
